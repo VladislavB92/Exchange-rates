f2ca31cd5b23661f36d48452f2479ab923030e97
Vladislavs Buzinskis <intelifoxgames@gmail.com>
2020-11-04 22:17:21 +0200

    Fixed data stack bug. Finished project.

--- app/Controllers/CurrencyRateController.php
+++ app/Controllers/CurrencyRateController.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 
-use App\Services\CurrencyRatePullService;
+use App\Services\CurrencyRateService;
 
 class CurrencyRateController
 {
     public function index()
     {
-        $currencyRate = new CurrencyRatePullService();
+        $currencyRate = new CurrencyRateService();
         $actualStats = $currencyRate->execute();
         
         return require_once __DIR__  . '/../Views/MainView.php';
---
+++ app/Services/CurrencyRateService.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Repositories\CurrencyDataRepository;
+use App\Managers\DatabaseManager;
+use App\Managers\XmlManager;
+
+class CurrencyRateService
+{
+    private CurrencyDataRepository $currencyDataRepository;
+
+    public function __construct()
+    {
+        $this->currencyDataRepository = new CurrencyDataRepository();
+    }
+
+    public function execute()
+    {
+        if (empty($this->currencyDataRepository->getAllRates())) {
+            $data = (new XmlManager())->result();
+
+            foreach ($data[1]['value'] as $currencyData) {
+                $currencyValues[] = [$currencyData['value'][0]['value'], $currencyData['value'][1]['value']];
+            }
+
+            foreach ($currencyValues as $rate) {
+                DatabaseManager::query()
+                    ->insert('rates')
+                    ->values([
+                        'currency' => ':currency',
+                        'rate' => ':rate'
+                    ])
+                    ->setParameters([
+                        'currency' => $rate[0],
+                        'rate' => $rate[1]
+                    ])
+                    ->execute();
+            }
+        }
+        return $this->currencyDataRepository->getAllRates();
+    }
+}
